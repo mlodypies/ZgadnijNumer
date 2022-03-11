23ae6963b3eac5a246072218d93d3f502fcbbe8f
3110180 <3110180@TE-GDA-D-D26F05>
2022-03-11 15:15:12 +0100

    Dodaj pliki projektów.

---
+++ mlodydog/Program.cs
@@ -0,0 +1,33 @@
+﻿Random r = new Random();
+int licz = r.Next(1, 11);
+int licz2;
+Console.WriteLine("Losowanie liczby 1-10");
+Console.WriteLine("Sprobuj zgadnac jaka to liczba");
+
+while (true)
+{
+    licz2 = Convert.ToInt32(Console.ReadLine());
+    if (licz2 < licz)
+    {
+        Console.WriteLine("Liczba ktora wpisales jest wieksza niz wylosowana.");
+        Console.WriteLine("Sprobuj jeszcze raz.");
+        continue;
+    }
+    else
+    if (licz2 > licz)
+    {
+        Console.WriteLine("Liczba ktora wpisales jest nizsza niz wylosowana.");
+        Console.WriteLine("Sprobuj ponownie.");
+        continue;
+
+    }
+    if (licz2 == licz)
+    {
+        Console.WriteLine("Gratuluje!");
+        Console.WriteLine("Wylosowana liczba to " + licz);
+        break;
+        
+    }
+}
+
+
